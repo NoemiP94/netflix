7764431c6012d664cfd5ce0d227d0a82c2be8ea8
NoemiP94 <m_emi94@hotmail.it>
2023-10-27 15:26:06 +0200

    finish finish =D

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           saga="The Lord of the Rings"
           movieTitle="the%20lord%20of%20the%20rings"
         />
-        <Movies saga="Dungeons & Dragons Movies" movieTitle="dungeons" />
+        <Movies saga="Doctor Who" movieTitle="doctor%20who" />
         <Movies saga="Marvel Cinematic Universe" movieTitle="marvel" />
       </main>
       <MyFooter />
